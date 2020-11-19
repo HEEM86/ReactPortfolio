cc6cf01c8d5fe93cb3cb3c4a92114007685b1a23
Slice Wess <ernestwesson86@gmail.com>
2020-11-18 23:09:13 -0800

    testing application with uncommented imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import App from './App.css';
+// import App from './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Jumbotron } from 'react-bootstrap';
-import Jumbo from './components/Jumbo';
+// import Jumbo from './components/Jumbo';
 import Footer from './components/Footer';
-import Navagation from './components/Navagation/Navagation';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Navagation from './components/Navagation/Navagation';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Contact from './components/Contact'
 import { Container, Card, Button, CardDeck } from 'react-bootstrap';
 
